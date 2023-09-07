ce055d17137732e782965b804df929560ed10a6b
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-09-08 00:56:11 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+# importacao de bibliotecas
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
+
+# entrar no facebook
+# clicar em novo registro
+# preenchimemto automatico
+# cadastro
